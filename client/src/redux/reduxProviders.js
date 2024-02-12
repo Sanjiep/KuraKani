0b39d2f1522b64180de3b0a4370873714cb08a2d
sanjiep.magar <saasmagar554@gmail.com>
2024-02-12 16:05:01 +0545

    redux persist added successfully

--- client/src/redux/reduxProviders.js
+++ client/src/redux/reduxProviders.js
@@ -1,16 +1,17 @@
 'use client'
 
 import { Provider } from 'react-redux';
-import store from './store';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const ReduxProvider = ({ children }) => {
     return (
         <Provider store={store} >
-            {/* <PersistGate loading={null} persistor={persistor}> */}
+            <PersistGate loading={null} persistor={persistor}>
                 
                 {children}
 
-            {/* </PersistGate> */}
+            </PersistGate>
         </Provider >
     )
 }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
+import userReducer from './reduceSlice/userSlice'
 import logger from 'redux-logger';
-import userSlice from './reduceSlice/userSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-export default configureStore({
-    reducer: {
-      user: userSlice
-    },
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+  const reducer = combineReducers({
+    user: userReducer,
+  })
+
+  const persistedReducer = persistReducer(persistConfig, reducer)
+
+  export const store = configureStore({
+    reducer: persistedReducer,
     middleware: ()=>[logger]
   });
+  
+  export const persistor = persistStore(store)
